9101930558c37322b2bece28d469413c438fc6f8
Jack Morrell <jackmorrell@jacks-mbp.speedport.ip>
2023-09-25 20:54:48 +0200

    updating javascript for counting

---
+++ app.js
@@ -0,0 +1,29 @@
+const pressBtn = document.querySelector(".press-btn");
+const upgradeBtn = document.querySelector(".upgrade-btn");
+const upgradeNum = document.querySelector(".upgrade-num");
+
+let numPress = 1;
+let numUpgrade = 10;
+
+pressBtn.addEventListener("click", function () {
+  const number = pressBtn.textContent;
+  pressBtn.textContent = Number(number) + numPress;
+});
+
+upgradeBtn.addEventListener("click", function () {
+  const numOne = pressBtn.textContent;
+  const numTwo = upgradeNum.textContent;
+  if (Number(numOne) < Number(numTwo)) {
+    return;
+  } else if (Number(numOne) >= Number(numTwo)) {
+    const num = pressBtn.textContent;
+    pressBtn.textContent = Number(num) - numUpgrade;
+    updateNum();
+    upgradeNum.textContent = numUpgrade;
+  }
+});
+
+const updateNum = function () {
+  numPress = numPress * 2;
+  numUpgrade = numUpgrade * 2;
+};
